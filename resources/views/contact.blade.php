721367a922d968c705eb42e9a5156e17ef97d488
Vladislaw Samsonov <valsailen96@gmail.com>
2022-10-12 23:20:23 +0300

    HW18

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,19 +9,33 @@
 
     <form action="/contact/submit" method="post">
 
+        @csrf
+
         <div class="form-group">
             <label for="email">input email</label>
             <input type="text" name="email" placeholder="input email " id="email" class="form-control">
+
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="number">input number</label>
             <input type="text" name="number" placeholder="input number " id="number" class="form-control">
+
+            @error('number')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="name">input name</label>
             <input type="text" name="name" placeholder="input name " id="name" class="form-control">
+
+            @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-success">Send</button>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/',[MainController::class, 'home']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [MainController::class, 'about']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [MainController::class, 'contact']);
+
+Route::post('/contact/submit', [\App\Http\Controllers\ContactController::class, 'submit']);
